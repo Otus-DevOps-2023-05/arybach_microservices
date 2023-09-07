8a3706a0aaec83454cadd6043fb596461e622206
Alex R <rybachek155@gmail.com>
2023-09-07 16:19:35 +0800

    tests expect ui, post-py and comments in both docker and src dirs

---
+++ src/comment/docker_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo `git show --format="%h" HEAD | head -1` > build_info.txt
+echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
+
+VERSION_TAG=$1
+
+docker build -t $USER_NAME/comment:$VERSION_TAG .
---
+++ src/post-py/docker_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo `git show --format="%h" HEAD | head -1` > build_info.txt
+echo `git rev-parse --abbrev-ref HEAD` >> build_info.txt
+
+VERSION_TAG=$1
+docker build -t $USER_NAME/post:$VERSION_TAG .
